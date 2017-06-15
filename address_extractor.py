0548e667830e376c1c8a6259091fecb0e251bb75
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-06-16 00:14:24 +0200

    Timer is now it's own class

--- address_extractor.py
+++ address_extractor.py
@@ -4,34 +4,30 @@ Extract data from kartverket's .csv-files
 (http://data.kartverket.no/download/content/geodataprodukter?korttype=3637&aktualitet=All&datastruktur=All&dataskema=All)
 and shrink it to only what is needed."""
 
-# TODO: use sets instead of lists for performanvehttps://stackoverflow.com/questions/7571635/fastest-way-to-check-if-a-value-exist-in-a-list
-
 import csv
 import json
 import os
 import threading
 from queue import Queue
 from time import time, strftime, gmtime
+from helpers import Timer
 
 
 def get_number_of_lines_in_file(file_name):
     """Return the number of lines in file."""
     with open(file_name) as f:
-        for i, l in enumerate(f):
+        for i, l in enumerate(f):  # noqa
             pass
         return i + 1
 
 
-def csv_reader(file_name, index, length, start_time, total_number_of_lines, lines_cycled, **kwargs):
+
+
+def csv_reader(file_name, timer, **kwargs):
     """Read a csv-file, and convert it to python-dictionary."""
-    time1 = time()
-    output_interval = 0.5
-    last_output = output_interval
-    this_time = time()
     data_list = []
     data_set = set()
-    lines_cycled_real = lines_cycled
-    number_of_lines = get_number_of_lines_in_file(file_name)
+
     with open(file_name) as csv_file:
         data = csv.reader(csv_file, **kwargs)
         for csv_row in data:
@@ -45,64 +41,62 @@ def csv_reader(file_name, index, length, start_time, total_number_of_lines, line
             if str(this_data) not in data_set:
                 data_list.append(this_data)
                 data_set.add(str(this_data))
-            time2 = time()
-            if time2 - time1 >= last_output:
-                total_time_spent = time2 - start_time
-                last_lines_cycled_real = lines_cycled_real
-                line_count = data.line_num
-                last_time = this_time
-                this_time = time2
-                lines_cycled_real = lines_cycled + line_count
-                print('\n{}/{} {} "{}"'.format(
-                    index + 1,
-                    length,
-                    strftime("%H:%M:%S", gmtime(total_time_spent)),
-                    file_name
-                ))
-                print(
-                    'Read and shrinked {:0.1f}% of this county after {}'.format(
-                        data.line_num / number_of_lines * 100,
-                        # this_time-time1,
-
-                        strftime("%H:%M:%S", gmtime(this_time - time1))
-                    ))
-                print('{:0.1f}% total, calculating this prosess should finish in {}, with an average of {:0.0f} lines/second'.format(
-                    lines_cycled_real / total_number_of_lines * 100,
-                    strftime("%H:%M:%S", gmtime(
-                        (total_number_of_lines - lines_cycled_real) / (
-                            (lines_cycled_real - last_lines_cycled_real) / (this_time - last_time))
-                    )),
-                    (lines_cycled_real - last_lines_cycled_real) /
-                    (this_time - last_time),
-                ))
-                last_output += output_interval
-    return data_list, number_of_lines
+            timer.update(data.line_num)
+    return data_list
 
 
-def read_csv_from_list_of_files(list_of_files, time1=time()):
+def read_csv_from_list_of_files(list_of_files, timer):
     """Read a list of csv-files."""
     adresser = []
-    total_number_of_lines = 0
-    lines_cycled = 0
-    for file_name in list_of_files:
-        total_number_of_lines += get_number_of_lines_in_file(file_name)
     for idx, county in enumerate(list_of_files):
-        a, number_of_lines = csv_reader(
-            county, idx, len(counties), time1, total_number_of_lines, lines_cycled, delimiter=';')
+        timer.keys['filename'] = county
+        timer.keys['filename'] = county
+        timer.keys['this_part'] = idx
+        timer.keys['this_total'] = get_number_of_lines_in_file(county)
+        a = csv_reader(
+            county, timer, delimiter=';')
         adresser.extend(a)
-        lines_cycled += number_of_lines
-        time3 = time()
+        timer.starting_progress += timer.keys['this_total']
         if idx == len(list_of_files) - 1:
-            print("{:0.1f} seconds elapsed total".format((time3 - time1)))
+            print("{:0.1f} seconds elapsed total".format(
+                (time() - timer.start_time)))
     return adresser
 
 
 if __name__ == '__main__':
     directory = 'data'
     counties = []
-    for file in os.listdir(directory):
-        if file.endswith(".csv"):
-            counties.append(os.path.join(directory, file))
+    total_number_of_lines = 0
+    data_file_size = 0
+    for file_name in os.listdir(directory):
+        if file_name.endswith(".csv"):
+            path = os.path.join(directory, file_name)
+            counties.append(path)
+            total_number_of_lines += get_number_of_lines_in_file(path)
+            data_file_size = os.path.getsize(path)
+
+    timer = Timer(keys={
+        'parts': len(counties),
+        'total': total_number_of_lines
+    })
+    timer.output_string = \
+        '\n{}/{} {} "{}"' +\
+        '\nRead and shrinked {:0.1f}% of this county after {}' +\
+        '\n{:0.1f}% total, calculating this prosess should finish in {}, with an average of {:0.0f} lines/second'
+    timer.output_format = lambda timer: (
+        timer.keys['this_part'] + 1,
+        timer.keys['parts'],
+        strftime("%H:%M:%S", gmtime(timer.total_time_spent)),
+        timer.keys['filename'],
+        timer.progress / timer.keys['this_total'] * 100,
+        strftime("%H:%M:%S", gmtime(timer.now - timer.start_time)),
+        timer.lines_cycled_real / timer.keys['total'] * 100,
+        strftime("%H:%M:%S", gmtime(
+            (timer.keys['total'] - timer.lines_cycled_real) / (
+                (timer.lines_cycled_real - timer.last_lines_cycled_real) / (timer.now - timer.last_time))
+        )),
+        (timer.lines_cycled_real - timer.last_lines_cycled_real) /
+        (timer.now - timer.last_time),)
     # counties = [
     #     'data/P13_10_VEST-AGDER_Adresse.csv',
     #     # counties[12],
@@ -114,24 +108,31 @@ if __name__ == '__main__':
     print('''Converting and shrinking data from Kartverket, total {} counties.
            '''.format(len(counties)))
 
-    def read_and_shrink(queue_):
+    def read_and_shrink(queue_, timer):
         """Description."""
         output_file = 'data/adresser.json'
         # open(output_file, 'w').close()
         adresser = []
         adresser.append(read_csv_from_list_of_files(
-            counties))
+            counties, timer))
         with open(output_file, 'w') as fout:
             json.dump(adresser[0], fout)
         print('Data saved in "{}"'.format(output_file))
         os.system('say "your program has finished"')
+        json_size = os.path.getsize(output_file)
+        print('Original data was {} bytes.\nNew json is {} bytes. \nShrinked to {:0.0f}%.'
+              .format(
+                  data_file_size,
+                  json_size,
+                  json_size / total_number_of_lines
+              ))
         queue.put('')
 
     queue = Queue()
     thread = threading.Thread(
         target=read_and_shrink,
         name='Thread1',
-        args=[queue]
+        args=[queue, timer]
     )
     thread.daemon = True
     thread.start()
