2488f45554a0b7b48e25b77c7838339e9f4d014a
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-07-10 19:24:47 +0200

    get_location_from_street_name

--- address_pymongo.py
+++ address_pymongo.py
@@ -1,9 +1,14 @@
 """Retrieval-functions for getting address-data from database"""
-print(__name__)
+import re
+
 if __name__ == '__main__':
     from model_pymongo import collection
+    from data.stupid_road_names import stupid_road_names
 else:
     from .model_pymongo import collection
+    from .data.stupid_road_names import stupid_road_names
+
+remove_house_number = re.compile(r'\s\d{0,3}\w$', re.IGNORECASE)
 
 
 def get_post_area_for_post_code(post_code):
@@ -37,6 +42,29 @@ def get_post_code_for_post_area(post_area):
     else:
         raise ValueError(
             'Did not find an address matching {}'.format(post_area))
+
+
+def filter_out_housenumber_from_street_name(street_name_with_house_number):
+    """Return a street_name without a housenumber."""
+    street_name_with_house_number = street_name_with_house_number.strip().lower()
+    if street_name_with_house_number in stupid_road_names:
+        print('fuck')
+        return street_name_with_house_number
+    return re.sub(remove_house_number, '', street_name_with_house_number)
+
+
+def get_location_from_address(street_name_with_house_number, post_code):
+    """Return a geolocation from a street_name (ignoring housenumber)."""
+    street_name = filter_out_housenumber_from_street_name(
+        street_name_with_house_number)
+    print(street_name)
+    address = collection.find_one(
+        {
+            'street_name': street_name,
+            'post_code': post_code
+        }
+    )
+    return address['loc']
 
 
 def get_address_from_street_name(
@@ -102,8 +130,20 @@ def get_address_from_street_name(
             'Did not find an address matching {}'.format(street_name))
 
 
+def generate_list_of_stupid_road_names():
+    """This is a list of problematic road names."""
+    addresses = collection.find(
+        {
+            'street_name': {'$regex': r"\s\d{1,4}$",
+                            '$options': 'i'},
+        }
+    )
+    d = []
+    for p in addresses:
+        d.append(p['street_name'])
+    return d
+
 if __name__ == '__main__':
-    result = get_address_from_street_name(
-        'r', near_geo=[58.13671436651572, 7.954736873662188])
-    for r in result:
-        print("{} {}\n{}".format(r['street_name'], r['post_code'], r['loc']))
+    result = get_location_from_address('Kongens gate 122b', 4610)
+    print(result)
+    # print(generate_list_of_stupid_road_names())
