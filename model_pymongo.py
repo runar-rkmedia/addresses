3b485def77a476652ed6179ec4f77233e708f2bc
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-09-15 16:01:27 +0200

    Add environement-variable for database.

--- model_pymongo.py
+++ model_pymongo.py
@@ -1,9 +1,12 @@
 """pymongo setup."""
 import os
-from pymongo import MongoClient,GEO2D
 
-url = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/testdb')
-database = os.getenv('MONGO_ADRESS_DB', 'address_database')
+from pymongo import GEO2D, MongoClient
+
+url = os.getenv('MONGODB_URI', os.getenv(
+    'MONGODB_ADDON_URI', 'mongodb://localhost:27017/testdb'))
+database = os.getenv(
+    'MONGO_ADRESS_DB', os.getenv('MONGODB_ADDON_DB', 'address_database'))
 client = MongoClient(url)
 db = client[database]
 collection = db.address_collection
