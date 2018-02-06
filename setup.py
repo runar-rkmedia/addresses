60cac9f0d87276eb0dc28b626b060b8c1adddfd1
Runar Kristoffersen <public@rkmedia.no>
2018-02-06 13:15:17 +0100

    add setup, minor improvements, a few more fields

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    # This is the name of your PyPI-package.
+    name='norwegian-adresses',
+    version='0.1.07',                          # Update the version number for new releases
+    # The name of your scipt, and also the command you'll be using for calling it
+    author="Runar Kristoffersen",
+    author_email="runar@rkmedia.no",
+    url="https://github.com/runar-rkmedia/addresses",
+    packages=['norwegian_adresses'],
+    install_requires=[
+        "pymongo==3.4.0",
+    ]
+
+)
